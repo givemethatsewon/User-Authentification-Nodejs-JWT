d3b438b515fddada939f8443859b342878a019c0
Seown Min <nvpz1598@gamil.com>
2023-11-25 23:08:18 +0900

    checked application is working properly

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+
+const posts = [
+    {
+        username: 'Kyle',
+        title: 'Post 1'
+    },
+    {
+        username: 'Jim',
+        title: 'Post 2'
+    }
+]
+
+app.get('/posts', (req, res) => {
+    res.json(posts)
+}) //checked application is working properly
+
+app.get('/login', (req, res) => {
+
+})
+
+app.listen(3000)
