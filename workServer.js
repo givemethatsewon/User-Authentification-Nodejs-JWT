e7575500157ecd559a2e79efde642bd40401b532
Seown Min <nvpz1598@gamil.com>
2023-11-26 03:30:20 +0900

    separate authServer and workServer

---
+++ workServer.js
@@ -0,0 +1,49 @@
+//!workServer는 post 생성, 삭제 등 api요청과 관련된 일만 처리
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const jwt = require('jsonwebtoken')
+//json을 login route에 pass 해야함. server가 핸들링 할 수 있도록 등록
+app.use(express.json())
+
+const posts = [
+    {
+        username: 'Kyle',
+        title: 'Post 1'
+    },
+    {
+        username: 'Jim',
+        title: 'Post 2'
+    }
+]
+
+//미들웨어로 authenticateToken 등록
+app.get('/posts', authenticateToken, (req, res) => {
+    // 미들웨어 때문에 user에 접근 가능
+    res.json(posts.filter(post => post.username === req.user.name))
+}) //checked application is working properly
+
+
+function authenticateToken(req, res, next)  {
+    /* 
+    get the token that they send us
+    verify that this is the correct user
+    return that user into the other function to get post
+    */
+    // token is going to come from the header, header -> Bearer
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1] // -> having authHeader, return token portion from that. if not don't do anything
+    if (token == null) 
+        return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err)
+            return res.sendStatus(403)
+        req.user = user //adding a user property to the req(request) object. attach user information to the request object
+        next()
+    })
+}
+
+const port = 4001
+app.listen(port, () => console.log(`listening on http://localhost:${port}`))
